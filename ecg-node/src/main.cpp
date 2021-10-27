70aa0626afcfae50f6bcf80accf40384746429e8
Koci0 <tomasz.kot13@gmail.com>
2021-10-27 17:23:25 +0200

    Add leads, ecg time and ecg value characteristics to node

--- ecg-node/src/main.cpp
+++ ecg-node/src/main.cpp
@@ -4,13 +4,37 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-const char *nameOfPeripheral = "Heart Rate Monitor";
-const char *uuidOfService = "00001800-fc88-4a2f-a932-b65010875819";
-const char *uuidOfCharacteristic = "00002A3D-fc88-4a2f-a932-b65010875819";
+typedef int lead_t;
+typedef int ecg_t;
+
+const char *peripheralName= "Heart Rate Monitor";
+const char *uuidService = "00001800-fc88-4a2f-a932-b65010875819";
+// Leads connected/disconnected
+const char *uuidLeadsCharacteristic = "00002A3D-fc88-4a2f-a932-b65010875819"; // 123
+// ECG
+const char *uuidEcgTimeCharacteristic = ""; // 456
+const char *uuidEcgValueCharacteristic = ""; // 789
 
 BLEServer *pServer;
 BLEService *pService;
-BLECharacteristic *pCharacteristic;
+BLECharacteristic *pLeadsCharacteristic;
+BLECharacteristic *pEcgTimeCharacteristic;
+BLECharacteristic *pEcgValueCharacteristic;
+
+lead_t getLeadsData()
+{
+    return 1;
+}
+
+time_t getEcgTimeData()
+{
+    return 2;
+}
+
+ecg_t getEcgValueData()
+{
+    return 3;
+}
 
 void setup()
 {
@@ -18,16 +42,29 @@ void setup()
     while (not Serial)
         ;
     Serial.println("Staring BLE...");
-    BLEDevice::init(nameOfPeripheral);
+    BLEDevice::init(peripheralName);
     pServer = BLEDevice::createServer();
-    pService = pServer->createService(uuidOfService);
-    pCharacteristic = pService->createCharacteristic(
-        uuidOfCharacteristic,
-        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    pCharacteristic->setValue("Hello world");
+    pService = pServer->createService(uuidService);
+
+    // Setup Leads
+    Serial.println("Setting up Leads");
+    pLeadsCharacteristic = pService->createCharacteristic(
+        uuidLeadsCharacteristic,
+        BLECharacteristic::PROPERTY_WRITE);
+
+    // Setup Ecg
+    Serial.println("Setting up ECG");
+    pEcgTimeCharacteristic = pService->createCharacteristic(
+        uuidEcgTimeCharacteristic,
+        BLECharacteristic::PROPERTY_WRITE);
+    pEcgValueCharacteristic = pService->createCharacteristic(
+        uuidEcgValueCharacteristic,
+        BLECharacteristic::PROPERTY_WRITE);
+
     pService->start();
+
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(uuidOfService);
+    pAdvertising->addServiceUUID(uuidService);
     pAdvertising->setScanResponse(true);
     BLEDevice::startAdvertising();
 
@@ -36,11 +73,21 @@ void setup()
 
 void loop()
 {
+    Serial.println("####################");
     while (true)
     {
-        auto data = pCharacteristic->getValue();
-        Serial.print("getValue: ");
-        Serial.println(data.c_str());
+        lead_t leadsData = getLeadsData();
+        time_t ecgTimeData = getEcgTimeData();
+        ecg_t ecgValueData = getEcgValueData();
+
+        Serial.print("Leads:\t");
+        Serial.println(leadsData);
+        Serial.print("EcgTime:\t");
+        Serial.println(ecgTimeData);
+        Serial.print("EcgValue:\t");
+        Serial.println(ecgValueData);
+        Serial.println("####################");
+
         delay(1000);
     }
 }
